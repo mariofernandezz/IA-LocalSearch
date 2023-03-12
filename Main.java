350b131f52ba44f9eb5e897690cda296185b7b38
mario.fernandez.simon <mario.fernandez.simon@estudiantat.upc.edu>
2023-03-12 18:05:34 +0100

    Esqueleto del main

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Nombre_de_la_clase_estado estado = new Nombre_de_la_clase_estado(N, M, seed);
+        // estado.solucionInicial1();
+        // estado.solucionInicial2();
+
+        //**************** Estado Inicial *********************
+        // print del estado inicial
+
+        // Problem problem = new Problem(estado, new SuccessorFunction(), new GoalTest(), new heuristicFunction());
+        // Search search = new ...;
+        //Search agent = new SearchAgent(problem, search);
+
+        //**************** Estado Finale **********************
+        // print del estado final
+    }
+}
